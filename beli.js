9e61e0883d711d969724d85149e050f5505ad1c3
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-20 12:39:33 +0700

    release 0

--- beli.js
+++ beli.js
@@ -2,11 +2,12 @@ function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+    if (kembalian >= 0) {
+      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya Rp.${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+      console.log(`uang gk cukup nih buat beli ${obj.item}. Kurang Rp.${Math.abs(kembalian)}`);
+      return;
       cb(0)
     }
   }, obj.waktu);
